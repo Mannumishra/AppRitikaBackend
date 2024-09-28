1cbcceb680e8df436c022816d3f5726b47391d81
mannumishra <mannumishra2207@outlook.com>
2024-09-28 16:11:55 +0530

    add chnages

--- Middleware/Multer.js
+++ Middleware/Multer.js
@@ -1,8 +1,14 @@
 const multer = require('multer')
+const fs = require("fs")
+const path = require("path")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './Public')
+        const dir = path.join(__dirname, 'Public'); // Use path.join for better path handling
+        if (!fs.existsSync(dir)) { // Check if directory exists
+            fs.mkdirSync(dir, { recursive: true }); // Create directory if it doesn't exist
+        }
+        cb(null, dir);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + file.originalname)
